6e3bf0485f2ec34f1fbb578de4f0f2ab71937991
salimsayed <salemsayed981@gmail.com>
2024-03-02 12:03:47 +0200

    Setup ENV

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import Welcome from "./screens/Welcome";
+import Home from "./screens/Home";
+import NFTDetails from "./screens/NFTDetails";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
     </View>
+  );
+}
+export default function App() {
+  const [fontLoaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+  });
+  if (!fontLoaded) return null;
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{ headerShown: true }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="NFT" component={NFTDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
